f87e5c5c574acb0394bfa6bd97989b5e00a3f8a4
woai3c <411020382@qq.com>
2020-11-06 20:30:06 +0800

    chore(main): 测试 pre-push 钩子

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 module.exports = function add(a, b) {
-    if (typeof a != 'number' || typeof b != 'number') {
-        throw new TypeError('参数必须为数值型')
-    }
+    // if (typeof a != 'number' || typeof b != 'number') {
+    //     throw new TypeError('参数必须为数值型')
+    // }
 
     return a + b
 }
