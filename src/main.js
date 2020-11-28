5ed05881f26f4072051b68787720a9fb24f6c0ec
woai3c <411020382@qq.com>
2020-11-28 23:07:50 +0800

    test: 添加一个 abs() 测试用例

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-module.exports = function add(a, b) {
+function add(a, b) {
     if (typeof a != 'number' || typeof b != 'number') {
         throw new TypeError('参数必须为数值型')
     }
 
     return a + b
+}
+
+function abs(a) {
+    if (typeof a != 'number') {
+        throw new TypeError('参数必须为数值型')
+    }
+
+    if (a < 0) return -a
+    return a
+}
+
+module.exports = {
+    add,
+    abs,
 }
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,13 @@
-const add = require('../src/main')
+const { add, abs } = require('../src/main')
 
 test('add', () => {
     expect(add(10, 10)).toBe(20)
     expect(() => add('10', {})).toThrow(TypeError)
+})
+
+test('abs', () => {
+    expect(abs(1)).toBe(1)
+    expect(abs(0)).toBe(0)
+    expect(abs(-1)).toBe(1)
+    expect(() => abs('abc')).toThrow(TypeError)
 })
